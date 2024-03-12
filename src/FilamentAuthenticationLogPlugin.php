038e50881eedb774a44525d3280ca09ed7879fbe
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2024-03-13 02:32:51 +0530

    Option to pass custom panelName, showing name of Actual user name instead of Class Name

--- src/FilamentAuthenticationLogPlugin.php
+++ src/FilamentAuthenticationLogPlugin.php
@@ -3,10 +3,13 @@
 namespace Tapp\FilamentAuthenticationLog;
 
 use Filament\Contracts\Plugin;
+use Filament\Facades\Filament;
 use Filament\Panel;
 
 class FilamentAuthenticationLogPlugin implements Plugin
 {
+    protected ?string $panelName = null;
+
     public static function make(): static
     {
         return app(static::class);
@@ -28,5 +31,22 @@ class FilamentAuthenticationLogPlugin implements Plugin
     public function boot(Panel $panel): void
     {
         //
+    }
+
+    public static function get(): Plugin
+    {
+        return filament(app(static::class)->getId());
+    }
+
+    public function getPanelName(): ?string
+    {
+        return $this->panelName ?? Filament::getCurrentPanel()->getId();
+    }
+
+    public function panelName(string $name): static
+    {
+        $this->panelName = $name;
+
+        return $this;
     }
 }
