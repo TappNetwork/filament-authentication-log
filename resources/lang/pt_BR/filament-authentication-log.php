056e124033ba1bd23a2a362f3dd7a12a1c0a7c7b
Samuel Terra <samuelterra22@gmail.com>
2025-07-22 09:16:51 -0300

    Add authentication log localization in Portuguese

---
+++ resources/lang/pt_BR/filament-authentication-log.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'navigation.group' => 'Logins',
+
+    'navigation.authentication-log.label' => 'Log de Autenticação',
+    'navigation.authentication-log.plural-label' => 'Logs de Autenticação',
+
+    'table.heading' => 'Logs de Autenticação',
+    'table.empty' => 'Nenhum log de autenticação encontrado',
+
+    'column.authenticatable' => 'Autenticável',
+    'column.ip_address' => 'Endereço IP',
+    'column.user_agent' => 'Agente do Usuário',
+    'column.login_at' => 'Data de Login',
+    'column.login_successful' => 'Login Bem-sucedido',
+    'column.logout_at' => 'Data de Logout',
+    'column.cleared_by_user' => 'Limpo pelo Usuário',
+];
