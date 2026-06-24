3227e2ddcac3eb47b2841b2759e2a6ca8459f49e
HuanXiang <qq418610675@gmail.com>
2026-06-24 22:37:36 +0800

    Add chinese translations (#52)
    - Add zh_CN, zh_TW, and zh_HK language files with full translations

---
+++ resources/lang/zh_CN/filament-authentication-log.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'navigation.group' => '登录',
+
+    'navigation.authentication-log.label' => '登录日志',
+    'navigation.authentication-log.plural-label' => '登录日志',
+
+    'table.heading' => '登录日志',
+    'table.empty' => '暂无登录日志',
+
+    'column.authenticatable' => '用户',
+    'column.ip_address' => 'IP 地址',
+    'column.user_agent' => '浏览器/设备',
+    'column.login_at' => '登录时间',
+    'column.login_successful' => '登录是否成功',
+    'column.logout_at' => '退出登录时间',
+    'column.cleared_by_user' => '由用户清除',
+];
---
+++ resources/lang/zh_TW/filament-authentication-log.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'navigation.group' => '登入',
+
+    'navigation.authentication-log.label' => '登入紀錄',
+    'navigation.authentication-log.plural-label' => '登入紀錄',
+
+    'table.heading' => '登入紀錄',
+    'table.empty' => '目前沒有登入紀錄',
+
+    'column.authenticatable' => '使用者',
+    'column.ip_address' => 'IP 位址',
+    'column.user_agent' => '瀏覽器/裝置',
+    'column.login_at' => '登入時間',
+    'column.login_successful' => '登入是否成功',
+    'column.logout_at' => '登出時間',
+    'column.cleared_by_user' => '由使用者清除',
+];
