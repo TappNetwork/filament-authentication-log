8211bf694f43fe46655739144f6cb80a193a20e9
JimmyLi <qq418610675@gmail.com>
2026-06-24 12:17:12 +0800

    Add chinese translations
    - Add zh_CN, zh_TW, and zh_HK language files with full translations

---
+++ resources/lang/zh_HK/filament-authentication-log.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'navigation.group' => '登入',
+
+    'navigation.authentication-log.label' => '登入記錄',
+    'navigation.authentication-log.plural-label' => '登入記錄',
+
+    'table.heading' => '登入記錄',
+    'table.empty' => '暫時沒有登入記錄',
+
+    'column.authenticatable' => '用戶',
+    'column.ip_address' => 'IP 位址',
+    'column.user_agent' => '瀏覽器/裝置',
+    'column.login_at' => '登入時間',
+    'column.login_successful' => '登入是否成功',
+    'column.logout_at' => '登出時間',
+    'column.cleared_by_user' => '由用戶清除',
+];
